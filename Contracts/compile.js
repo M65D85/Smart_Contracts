a6c9815ea96242ae2e366fe796c02c1d24ff3cee
M65D85 <34279492+M65D85@users.noreply.github.com>
2018-07-20 03:09:38 +0530

    Delete compile.js

--- Contracts/compile.js
+++
@@ -1,8 +0,0 @@
-const path = require("path");
-const fs = require("fs");
-const solc = require("solc");
-
-const contractPath = path.resolve(__dirname, "Contracts", "Lottery.sol");
-const source = fs.readFileSync(contractPath, "utf8");
-
-module.exports = solc.compile(source, 1).contracts[":Lottery"];
